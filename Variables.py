8417441486fe8b4836aff257deced68c75f5b170
CodeNameAllie <18amulligan@iaa.edu.ec>
2015-05-07 22:07:28 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,44 @@
+name = "Rosey Posey" #string 
+age = 5 #integer
+gpa = 2.8 #floatingpoint
+
+print name 
+print age
+print gpa
+
+print 'My name is', name, '. I am', age, 'years old.'
+print 'In ten years, I will be', age + 10
+
+#Dog years
+ageInHumanYears = 3
+ageInDogYears = ageInHumanYears * 7
+
+print 'My dog is', ageInDogYears, 'years old in Dog Years.'
+print 'But my dog is', ageInHumanYears, 'Years old in Human Years.'
+
+#Order of Operations
+#Parenthesis, Exponents, Multiplication, Division, Subtraction, Addition
+myNumber = 5 + 4 * 5
+secondNumber = 5 * 4 + 5
+
+print myNumber
+print secondNumber
+
+print "The numbers are the same."
+
+number = 5 + 4 * 7
+numberTwo = 5 * 4 + 7
+
+print number 
+print numberTwo
+
+print "They are different because of the operations, except in certain cases"
+print "like 5, 4, and 5 since they will always equal the same number when added or multiplied"
+
+#print "Hello" + 2
+print "Hello", 2
+
+import math
+root = math.sqrt(81)
+
+print root
