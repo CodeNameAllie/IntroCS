ac783c69c12f014d7f7bc85fafca0192704160df
CodeNameAllie <18amulligan@iaa.edu.ec>
2015-05-07 22:05:43 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+print "Hello World"
+print "My name is Alassandra"
+print "I am a teenager \nThis is python"
+
+print 2015 + 1 #Printing an integer
+print "2015"
+
+print 3.1416 #Printing a floating point number
+
+#Math Opperations
+print 78 + 32
+print 910 - 86
+print 26 * 9
+print 100 / 25 
+
+#Modules
+print 10%3
+
+print 17 % 2
+print 16 % 2 
