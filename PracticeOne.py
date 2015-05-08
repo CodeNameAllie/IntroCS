beeb893905d526f6be007f8bd23ddf19d779d264
CodeNameAllie <18amulligan@iaa.edu.ec>
2015-05-07 22:06:52 -0500

    Create PracticeOne.py

---
+++ PracticeOne.py
@@ -0,0 +1,17 @@
+#practice variables
+name = "Julia"
+lastname = "DelaCruz"
+address = "5 Holtz Ct. Alameda, California"
+email = "juju214gmail.com"
+phonenumber = "510-551-5006"
+
+#Printing variables
+print name
+print lastname
+print address
+print email
+print phonenumber
+
+#string 
+print "My name is", name, lastname, ". My address is", address, "."
+print "You can reach me at", email, " or by calling me using this number", phonenumber
