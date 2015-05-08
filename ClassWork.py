90d7ec25be8a94d480c40ccc4d63bba0cfcf2620
CodeNameAllie <18amulligan@iaa.edu.ec>
2015-05-07 22:08:36 -0500

    Create ClassWork.py

---
+++ ClassWork.py
@@ -0,0 +1,21 @@
+mystring = "Hello" 
+myint = 20  
+myfloat = 10 
+
+#print
+print mystring
+print myint
+print myfloat
+
+#mytext 
+print mystring,"I am", myint, "but I can pass as", myfloat, "years old." 
+
+#Order of Operations
+#Parenthesis, Exponents, Multiplication, Division, Subtraction, Addition
+myNumber = 5 + 4 * 10
+secondNumber = 5 * 4 + 10
+
+print myNumber
+print secondNumber
+
+print myNumber, "OR", secondNumber, "depending on where the opertion is placed." 
