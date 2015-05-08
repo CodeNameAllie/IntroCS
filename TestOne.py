bcdaa19212ce73f3e3712d8ca031c7127afe3348
CodeNameAllie <18amulligan@iaa.edu.ec>
2015-05-07 22:08:58 -0500

    Create TestOne.py

---
+++ TestOne.py
@@ -0,0 +1,11 @@
+operand1 = 95
+operand2 = 64.5 
+
+print operand1 + operand2
+print operand1 - operand2
+print operand1 * operand2
+print operand1 / operand2
+print operand1 % operand2
+
+#I got my solution by using operand1 and operand2 and 
+#combining them in mathematical equations
