31e923f7b52109724fd691aa06c102dc9fcf650c
CodeNameAllie <18amulligan@iaa.edu.ec>
2015-05-26 09:44:14 -0500

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,18 @@
+#wrong
+#name = input("What is your name? ")
+
+#right
+name = raw_input("What is your name? ")
+last = raw_input("What is your last name? ")
+
+print "Your name is", name, last
+
+age = input("How old are you? ")
+
+print "You are", age, "years old."
+print "In ten years you will be", age + 10
+
+print age * 7
+print name * 7
+
+#LOL Allie Allie Allie Allie Allie Allie Allie xD
