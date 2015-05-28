609c9aaf26664c7c851d6bee2b1bfc41a7bcb348
CodeNameAllie <18amulligan@iaa.edu.ec>
2015-05-28 10:00:14 -0500

    Create Homework1.py

---
+++ Homework1.py
@@ -0,0 +1,14 @@
+
+#print first name
+#print last name
+#print your year of birth
+#print your year of graduation
+#print your GPA
+
+import sys
+
+print "Your name is: ", sys.argv[1]
+print "Your last name is: ", sys.argv[2]
+print "Your year of birth is: ", sys.argv[3]
+print "Your year of graduation is: ", sys.argv[4]
+print "Your GPA is: ", sys. argv[5]
